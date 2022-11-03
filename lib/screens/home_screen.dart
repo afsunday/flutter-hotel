41726fd56ab0e4a48491610727ed7e34958119c1
afuwape <afuwapesunday12@gmail.com>
2022-11-03 01:38:56 +0100

    Added safe area to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,87 +15,89 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styles.bgColor,
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-              const Gap(20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
                 children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Good Morning', style: Styles.headLineStyle4),
-                        const Gap(5),
-                        Text('Book Ticket', style: Styles.headLineStyle2)
-                      ]),
-                  Container(
-                    height: 50.0,
-                    width: 50.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12.0),
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/images/student.png"),
+                const Gap(20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Good Morning', style: Styles.headLineStyle4),
+                          const Gap(5),
+                          Text('Book Ticket', style: Styles.headLineStyle2)
+                        ]),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/student.png"),
+                        ),
                       ),
                     ),
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD)
                   ),
-                ],
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205)),
+                      Text('search', style: Styles.headLineStyle4)
+                    ]
+                  ),
+                ), 
+                const Gap(30),
+                const AppDoubleTextWidget(bigText:'Upcoming Flights', smallText: 'View all')
+              ],
               ),
-              const Gap(25),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xFFF4F6FD)
-                ),
-                child: Row(
-                  children: [
-                    const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205)),
-                    Text('search', style: Styles.headLineStyle4)
-                  ]
-                ),
-              ), 
-              const Gap(30),
-              const AppDoubleTextWidget(bigText:'Upcoming Flights', smallText: 'View all')
-            ],
             ),
-          ),
-          // Ticket View Second section
-          const Gap(15),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.only(left: 20),
-            child: Row(
-              children: ticketList.map((iTicket) => TicketView(ticket: iTicket)).toList()
+            // Ticket View Second section
+            const Gap(15),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: ticketList.map((iTicket) => TicketView(ticket: iTicket)).toList()
+              ),
             ),
-          ),
-          const Gap(25),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Hotels', style: Styles.headLineStyle2),
-                InkWell(
-                  child: Text('View all', style: Styles.textStyle.copyWith(color: Styles.primaryColor)), 
-                  onTap: () => { }
-                )
-              ]
+            const Gap(25),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Hotels', style: Styles.headLineStyle2),
+                  InkWell(
+                    child: Text('View all', style: Styles.textStyle.copyWith(color: Styles.primaryColor)), 
+                    onTap: () => { }
+                  )
+                ]
+              ),
             ),
-          ),
-          const Gap(15),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.only(left: 20),
-            child: Row(
-              children: hotelList.map((singleHotel) => HotelScreen(hotel: singleHotel)).toList()
+            const Gap(15),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: hotelList.map((singleHotel) => HotelScreen(hotel: singleHotel)).toList()
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),  
     );
   }
